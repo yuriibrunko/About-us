4d44ef382da648486ba405f622171424f156bcda
yuriibrunko <yurii.brunko13@gmail.com>
2019-05-29 20:41:38 +0300

    slick

--- js/index.js
+++ js/index.js
@@ -9,5 +9,24 @@
 $('.col').slick({
   slidesToShow: 4, //сколько слайдов показывать в карусели
   slidesToScroll: 4, // сколько слайдов прокручивать за раз
-  dots: true
+  arrows: false,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+  ]
 });
